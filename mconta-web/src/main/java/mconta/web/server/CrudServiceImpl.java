d6cdb69ad7ab8e93ce622264b66c4d9840cfc54a
cbmarc@gmail.com <cbmarc@gmail.com@46dfc908-3175-5661-df70-577876ab8126>
2011-12-22 23:02:43 +0000

    logging tests

--- mconta-web/src/main/java/mconta/web/server/CrudServiceImpl.java
+++ mconta-web/src/main/java/mconta/web/server/CrudServiceImpl.java
@@ -5,6 +5,8 @@ package mconta.web.server;
 
 import java.util.List;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import mconta.domain.dao.CrudDAO;
 import mconta.domain.model.Model;
@@ -19,6 +21,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 @SuppressWarnings("serial")
 public class CrudServiceImpl
 	extends SpringRemoteServiceServlet implements CrudService {
+	
+	private static Logger logger =
+		    Logger.getLogger(CrudServiceImpl.class.getName());
 		
 	@Autowired
 	protected CrudDAO<Model> dao;
@@ -50,6 +55,14 @@ public class CrudServiceImpl
 
 	public List<Model> getAll(String model) throws Exception {
 		List<Model> list = null;
+		
+		logger.severe("--- severe logging message");
+		logger.info("--- info logging message");
+		logger.warning("--- warning logging message");
+		logger.config("--- config logging message");
+		logger.fine("--- fine logging message");
+		logger.finer("--- finer logging message");
+		logger.finest("--- finest logging message");
 		
 		try {
 			list = dao.getAll(model);
