eab9c6337ec2b1f42028651038c8b801143a54de
cbmarc@gmail.com <cbmarc@gmail.com@46dfc908-3175-5661-df70-577876ab8126>
2011-12-27 06:03:29 +0000

    multimodule

--- trunk/mconta-web/src/main/java/mconta/web/client/Mconta.java
+++ trunk/mconta-web/src/main/java/mconta/web/client/Mconta.java
@@ -1,30 +1,14 @@
 package mconta.web.client;
 
 import mconta.web.client.presenter.impl.MainPresenter;
-import mconta.web.client.rpc.LoginService;
-import mconta.web.client.rpc.LoginServiceAsync;
 import mconta.web.client.view.impl.MainViewImpl;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class Mconta implements EntryPoint {
-	
-	private final LoginServiceAsync loginService = GWT
-			.create(LoginService.class);
 
 	public void onModuleLoad() {
-		
-		// DO LOGIN
-		/*loginService.login("anonymous", "", new AppAsyncCallback<UserDTO>() {
-
-			public void onSuccess(UserDTO result) {
-				new MainPresenter(new MainViewImpl()).go(RootPanel.get("mainContainer"));
-				
-			}
-			
-		});*/
 		
 		new MainPresenter(new MainViewImpl()).go(RootPanel.get("mainContainer"));
 		
---
+++ trunk/mconta-web/src/main/java/mconta/web/client/login/rpc/LoginServiceAsync.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package mconta.web.client.login.rpc;
+
+import mconta.web.shared.UserDto;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * @author Marc
+ *
+ */
+public interface LoginServiceAsync {
+	
+	void login(String email, String password, AsyncCallback<UserDto> callback);
+	void logout(AsyncCallback<Void> callback);
+	
+}
